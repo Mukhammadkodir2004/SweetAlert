5ea0847b5c491c19cf92da789b36204213f3df1d
Mukhammadkodir2004 <sattaraliyev10@gmail.com>
2023-03-11 10:12:39 +0500

    sweet alert

---
+++ JavaScript/java.js
@@ -0,0 +1,51 @@
+alert ("Lets go! To Apply the Stanford University. Are you Ready")
+
+var name = prompt('What is your name?')
+var surname = prompt('What is your surname?')
+var age = prompt('How old are you?')
+var country = prompt('Where are you from?')
+var email=prompt('Can you send us your email address?')
+var school = prompt('When did you start the school?')
+var graduation = prompt('Expectation of Graduate the school?')
+var certificate = prompt('Which type of the certificate do you have?')
+var con= confirm('Have you ever been apply to the Standford University?')
+var information = prompt('Where did you know about us?')
+
+
+
+console.log('Name:', name )
+console.log('Surname:', surname)
+console.log('Age:', age)
+console.log('Country:', country)
+console.log('Email address:',email)
+console.log('Date of the start  School:',school )
+console.log('Date of the Graduation the School:',graduation )
+console.log('Certificates:',certificate )
+console.log('Answer:',con )
+console.log('From:',information )
+
+alert(
+'Name:'+ name +
+'Surname:'+ surname + 
+'Age:'+ age+
+'Country:'+ country+ 
+'Email address:'+ email+
+'Date of the start  School:'+ school+
+'Date of the Graduation the School:'+ graduation+
+'Certificates:'+ certificate+
+'Answer:'+con+
+'From:'+ information 
+)
+
+
+
+
+
+swal({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success",
+    button: "Aww yiss!",
+  });
+
+ 
